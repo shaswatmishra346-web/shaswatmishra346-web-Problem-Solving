3d2b3fea81bf84580c0937cb8350c04e1cfc5945
Shaswat Mishra <shaswatmishra346@gmail.com>
2026-07-22 00:53:08 +0530

    Update 0003-longest-substring-without-repeating-characters.cpp

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int low = 0;
-        int maxS = 0;
-        unordered_map<char, int> f;
+    int characterReplacement(string s, int k) {
+        vector<int> freq(256, 0); 
+        int low = 0, maxCount = 0, maxLen = 0;
 
         for (int high = 0; high < s.size(); high++) {
-            f[s[high]]++;
-            while (f.size() < (high - low + 1)) {
-                f[s[low]]--;
-                if (f[s[low]] == 0) {
-                    f.erase(s[low]);
-                }
+            freq[s[high]]++; 
+            maxCount = max(maxCount, freq[s[high]]);
+
+            while ((high - low + 1) - maxCount > k) {
+                freq[s[low]]--;
                 low++;
             }
 
-            maxS = max(maxS, high - low + 1);
+            maxLen = max(maxLen, high - low + 1);
         }
 
-        return maxS;
+        return maxLen;
     }
 };
