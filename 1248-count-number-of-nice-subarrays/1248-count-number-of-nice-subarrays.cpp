400b63fd1bd5849d535eb451dab63d6c3e992244
Shaswat Mishra <shaswatmishra346@gmail.com>
2026-08-13 00:30:27 +0530

    Time: 8 ms (60.28%), Space: 71.6 MB (46.58%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return atMost(nums, k) - atMost(nums, k - 1); 
+        return atMost(nums, k) - atMost(nums, k - 1);
     }
-    
     int atMost(vector<int>& nums, int k) {
-        int s = 0, ans = 0;
-        for(int i = 0, j = 0; i < nums.size(); i++) {
+        if (k < 0) return 0;
+        int s = 0;
+        int ans = 0;
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
             s += nums[i] % 2;
-            while(s > k) s -= (nums[j++] % 2);
-            ans += (i - j + 1);
+
+            while (s > k) {
+                s -= nums[j] % 2;
+                j++;
+            }
+            ans += i - j + 1;
         }
         return ans;
     }
