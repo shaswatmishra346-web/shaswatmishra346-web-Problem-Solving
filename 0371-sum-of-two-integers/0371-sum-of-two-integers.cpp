1a096e99e55783f42a8265ed2bd1f93eb6437c07
Shaswat Mishra <shaswatmishra346@gmail.com>
2026-09-15 01:44:11 +0530

    Time: 0 ms (100%), Space: 7.9 MB (1.44%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while (b != 0) {
+            unsigned int c = a & b;
+            a ^= b;
+            b = c << 1;
+        }
+        return a;
+    }
+};
